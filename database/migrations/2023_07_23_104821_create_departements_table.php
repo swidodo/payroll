e6f3d51f3cf1a26420339a7a2df608ca3d047a29
Puguh Dwi Samdik <puguh@wilopocargo.com>
2023-07-24 10:57:53 +0700

    migration departements

---
+++ database/migrations/2023_07_23_104821_create_departements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->bigIncrements('department_head_id')->comment('employee_id');
+            $table->string('is_active', [0,1])->default(1);
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departements');
+    }
+};
