734f60476b840dc606dd5934daefac3f6de4b6c3
anggitadhikary <anggitadikari@gmail.com>
2023-08-04 13:34:54 +0700

    server-side Dayoff

--- app/Http/Controllers/DayoffController.php
+++ app/Http/Controllers/DayoffController.php
@@ -4,33 +4,75 @@ namespace App\Http\Controllers;
 
 use App\Models\Dayoff;
 use App\Models\ShiftSchedule;
+use App\Models\User;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\Facades\DataTables;
 
 class DayoffController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         if (Auth::user()->can('manage dayoff')) {
+            $branchId = Auth::user()->branch_id;
+            // dd($branchId);
+
+            // $dayoffs = Dayoff::where('created_by', $branchId)->get();
+
+            $dayoffs = DB::table('dayoffs')
+            ->whereIn('created_by', function ($query) use ($branchId) {
+                $query->select('id')
+                    ->from('users')
+                    ->where('branch_id', $branchId);
+            })
+            ->get();
+
+            // // Get user ids with the same branch_id
+            // $userIds = User::where('branch_id', $branchId)->pluck('id')->toArray();
+
+            // // Fetch Dayoff data based on the created_by (user_id) with the same branch_id
+            // $dayoffs = DB::table('dayoffs')
+            //     ->whereIn('created_by', $userIds)
+            //     ->get();
+
+            // dd($dayoffs);
             // $employees = Employee::where('created_by', '=', Auth::user()->creatorId())->get();
             // $shiftSchedules = ShiftSchedule::where('created_by', '=', Auth::user()->creatorId())->where('status', 'Approved')->orderBy('id', 'asc')->get();
-            $dayoffs = Dayoff::where('created_by', Auth::user()->creatorId())->get();
+            // $dayoffs = Dayoff::select('id', 'date')->get();
+            // dd($dayoffs);
+            if ($request->ajax()) {
+                return DataTables::of($dayoffs)->addIndexColumn()
+                    ->addColumn('company_dayoff', function ($dayoff) {
+                        return \Carbon\Carbon::parse($dayoff->date)->format('l') ?? '-';
+                    })
+                    ->addColumn('date', function ($dayoff) {
+                        return \Carbon\Carbon::parse($dayoff->date)->format('j F Y') ?? '-';
+                    })
+                    ->addColumn('action', function ($data) {
+                        $action = '';
+                        if (Auth()->user()->canany(['edit dayoff', 'delete dayoff'])) {
+                            $action = '<td class="text-center" >
+                                            <div class="dropdown dropdown-action" >
+                                                <a href = "#" class="action-icon dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="material-icons"> more_vert</i></a>
+                                                <div class="dropdown-menu dropdown-menu-right">';
+                            if (Auth()->user()->can('delete dayoff')) {
+                                $url_delete = route('dayoff.destroy', $data->id);
+                                $action .= '<a id="delete-dayoff-btn" data-url="' . $url_delete . '" class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#delete_dayoff"><i class="fa fa-trash-o m-r-5"></i> Delete</a>';
+                            }
 
-            $days = [
-                'Monday',
-                'Tuesday',
-                'Wednesday',
-                'Thursday',
-                'Friday',
-                'Saturday',
-                'Sunday',
-            ];
+                            $action .= '</div></div></td>';
+                        }
+                        return $action;
+                    })
+                    ->escapeColumns([])
+                    ->toJson();
+            }
 
-            return view('pages.contents.dayoff.index', compact('days', 'dayoffs'));
+            return view('pages.contents.dayoff.index');
         } else {
             toast('Permission denied.', 'error');
             return redirect()->route('dayoff.index');
