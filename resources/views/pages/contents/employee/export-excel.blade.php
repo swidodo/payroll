d6455d07d36f9250c9afc32fef0ec2bdf9ee1540
swidodo <smtwidodo04@gmail.com>
2023-10-08 19:35:26 +0700

    update

--- resources/views/pages/contents/employee/export-excel.blade.php
+++ resources/views/pages/contents/employee/export-excel.blade.php
@@ -3,40 +3,31 @@
 <table>
     <thead>
         <tr>
-            <th>Name</th>
-            <th>Identity Card Number</th>
-            <th>Family Card Number</th>
-            <th>NPWP Number</th>
-            <th>Dob</th>
-            <th>Gender</th>
-            <th>Phone</th>
-            <th>Address</th>
-            <th>Email</th>
-            <th>Password</th>
-            <th>Employe ID</th>
-            <th>Branch</th>
-            <th>Department</th>
-            <th>Position</th>
-            <th>Company Doj</th>
-            <th>Company Doe</th>
-            <th>Account Holder Name</th>
-            <th>Account Number</th>
-            <th>Bank Name</th>
-            <th>Bank Code</th>
-            <th>Branch Location</th>
-            <th>Tax payer ID</th>
-            <th>Salary Type</th>
-            <th>Salary</th>
-            <th>Net Salary</th>
-            <th>Is Active</th>
-            <th>Level Approval</th>
-            <th>Leave Type</th>
-            <th>Employee Type</th>
-            <th>Marital Status</th>
-            <th>Total Leave</th>
-            <th>Total Leave Remaining</th>
-            <th>Out Date</th>
-            <th>Status</th>
+            <th>NAME EMPLOYEE</th>
+            <th>IDENTITY CARD</th>
+            <th>FAMILY CARD</th>
+            <th>NPWP</th>
+            <th>BIRTH OF DATE</th>
+            <th>GENDER</th>
+            <th>RELIGION</th>
+            <th>MARITAL STATUS</th>
+            <th>PHONE</th>
+            <th>ADDRESS</th>
+            <th>EMAIL</th>
+            <th>EMPLOYEE ID</th>
+            <th>EMPLOYEE TYPE</th>
+            <th>WORK TYPE</th>
+            <th>DEPARTMENT CODE</th>
+            <th>POSITION CODE</th>
+            <th>ACCOUNT HOLDER NAME</th>
+            <th>BANK NAME</th>
+            <th>ACCOUNT NUMBER</th>
+            <th>BANK CODE</th>
+            <th>BRANCH BANK</th>
+            <th>STATUS</th>
+            <th>DATE OF JOINT</th>
+            <th>DATE OF RESIGN</th>
+            <th>BRANCH NAME</th>
         </tr>
     </thead>
     <tbody>
@@ -48,34 +39,25 @@
             <td>{{$employ->npwp_number}}</td>
             <td>{{$employ->dob}}</td>
             <td>{{$employ->gender}}</td>
+            <td>{{$employ->religion}}</td>
+            <td>{{$employ->marital_name}}</td>
             <td>{{$employ->phone}}</td>
             <td>{{$employ->address}}</td>
             <td>{{$employ->email}}</td>
-            <td>{{$employ->password}}</td>
             <td>{{$employ->employee_id}}</td>
-            <td>{{$employ->branch_name}}</td>
-            <td>{{$employ->departement_name}}</td>
-            <td>{{$employ->position_name}}</td>
+            <td>{{$employ->employee_type}}</td>
+            <td>{{$employ->work_type}}</td>
+            <td>{{$employ->departement_code}}</td>
+            <td>{{$employ->position_code}}</td>
+            <td>{{$employ->account_holder_name}}</td>
+            <td>{{$employ->bank_name}}</td>
+            <td>{{$employ->account_number}}</td>
+            <td>{{$employ->bank_identifier_code}}</td>
+            <td>{{$employ->bank_location}}</td>
+            <td>{{$employ->status}}</td>
             <td>{{$employ->company_doj}}</td>
             <td>{{$employ->company_doe}}</td>
-            <td>{{$employ->account_holder_name}}</td>
-            <td>{{$employ->account_number}}</td>
-            <td>{{$employ->bank_name}}</td>
-            <td>{{$employ->bank_identifier_code}}</td>
-            <td>{{$employ->branch_location}}</td>
-            <td>{{$employ->tax_payer_id}}</td>
-            <td>{{$employ->salary_type}}</td>
-            <td>{{$employ->salary}}</td>
-            <td>{{$employ->net_salary}}</td>
-            <td>{{ ($employ->is_active =='true') ? 'yes' : 'no'}}</td>
-            <td>{{$employ->level_approval}}</td>
-            <td>{{$employ->leave_type}}</td>
-            <td>{{$employ->employee_type}}</td>
-            <td>{{$employ->marital_name}}</td>
-            <td>{{$employ->total_leave}}</td>
-            <td>{{$employ->total_leave_remaining}}</td>
-            <td>{{$employ->out_date}}</td>
-            <td>{{$employ->status}}</td>
+            <td>{{$employ->branch_name}}</td>
         </tr>
     @endforeach
 </tbody>
