3532ba6bf832690d3c4ec9a9b187895b0b089fed
swidodo <smtwidodo04@gmail.com>
2023-10-11 17:37:57 +0700

    update ptkp

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -34,7 +34,7 @@ class PayrollController extends Controller
      */
     public function index()
     {
-        DB::table('take_home_pay')->delete();
+        DB::table('attendance_employees')->delete();
         if (Auth::user()->can('manage payroll')) {
             if (Auth::user()->type != 'company') {
                 $user     = Auth::user();
