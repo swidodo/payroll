b695401b464119c074c3b2a3c6a14f3ec46441ba
swidodo <smtwidodo04@gmail.com>
2023-10-12 15:49:55 +0700

    set number format

--- resources/views/pages/contents/payroll/index.blade.php
+++ resources/views/pages/contents/payroll/index.blade.php
@@ -388,10 +388,10 @@
 $(document).on('change','.editItemBpjs',function(){
     var id = $(this).attr('data-id');
     var empId = $(this).attr('data-empId');
-    var data = [
-        'id' => id,
-        'employee_id' => empId
-    ];
+    var data = { 
+        'id': id,
+        'employee_id': empId
+    }
     var bpjs =[];
     if(!($(this).prop("checked"))) {
         bpjs.push(data)
