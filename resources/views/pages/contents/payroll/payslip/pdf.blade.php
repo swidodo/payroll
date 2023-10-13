2469e6042750e30d472336bdc3870ad5ac199f1f
swidodo <smtwidodo04@gmail.com>
2023-10-13 23:38:27 +0700

    set pph21

--- resources/views/pages/contents/payroll/payslip/pdf.blade.php
+++ resources/views/pages/contents/payroll/payslip/pdf.blade.php
@@ -298,7 +298,7 @@
                         <tr> 
                             <td style="width: 18%;font-size: 14px">Position</td>
                             <td style="width: 2%">:</td>
-                            <td style=" font-size: 14px;text-align:start;font-weight: 400;"></td>
+                            <td style=" font-size: 14px;text-align:start;font-weight: 400;">{{ strtoupper($salary->position_name) }}</td>
                         </tr>
                     </tbody>
                 </table>
