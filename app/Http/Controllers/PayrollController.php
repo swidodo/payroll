95bc5bfd3cd51e90965f75197b70eeabf11b5e28
swidodo <smtwidodo04@gmail.com>
2023-10-13 08:10:13 +0700

    set pph21

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -29,9 +29,6 @@ class PayrollController extends Controller
 {
     public function index()
     {
-        DB::table('bpjs_value')->delete();
-        DB::table('take_home_pay')->delete();
-        DB::table('rekap_pph21s')->delete();
         if (Auth::user()->can('manage payroll')) {
             if (Auth::user()->type != 'company') {
                 $user     = Auth::user();
