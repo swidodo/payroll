f5fadef1cf42cd0dad5c9afbb8833900b1ea7c68
swidodo <smtwidodo04@gmail.com>
2023-10-15 20:26:17 +0700

    update error

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -259,17 +259,17 @@
         <!-- /Message Notifications -->
 
         <li class="nav-item dropdown has-arrow main-drop">
-           <!--  <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
+            <a href="#" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
                 <span class="user-img"><img src="assets/img/profiles/avatar-21.jpg" alt="">
                 <span class="status online"></span></span>
                 <span>{{Auth::user()->name}}</span>
-            </a> -->
+            </a>
             <div class="dropdown-menu">
                 <a class="dropdown-item" href="#">My Profile</a>
                 <a class="dropdown-item" href="#">Settings</a>
                 <form action="{{route('logout')}}" method="POST">
                 @csrf
-                <button type="submit" class="dropdown-item" href="settings.html">Logout</button>
+                <button type="submit" class="dropdown-item">Logout</button>
                 </form>
             </div>
         </li>
