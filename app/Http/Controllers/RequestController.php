c726e7cd71c68f93ba7cb8f42733d1524d090d6e
swidodo <smtwidodo04@gmail.com>
2023-10-17 06:34:57 +0700

    update errorss

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Branch;
 class RequestController extends Controller
 {
     public function index(){
