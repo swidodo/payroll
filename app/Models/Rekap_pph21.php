ea8c956f533e2facd3041af9401ffff76d3e9000
swidodo <smtwidodo04@gmail.com>
2023-10-17 01:46:33 +0700

    update errorss

--- app/Models/Rekap_pph21.php
+++ app/Models/Rekap_pph21.php
@@ -9,6 +9,7 @@ class Rekap_pph21 extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+    protected $table = 'rekap_pph21s';
     public function employee()
     {
         return $this->belongsTo(Employee::class, 'employee_id');
