e3b76a8a534606980196eeb0a5ef41ee985c3862
swidodo <smtwidodo04@gmail.com>
2023-10-17 03:36:44 +0700

    update errorss

--- resources/views/pages/contents/report/rekap_pph21/rekap_pph21_pdf.blade.php
+++ resources/views/pages/contents/report/rekap_pph21/rekap_pph21_pdf.blade.php
@@ -85,29 +85,29 @@
     <tbody>
          @foreach ($pph as $row)
         <tr>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->no_employee }}</td>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->name }}</td>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->position_name }}</td>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->salary_pokok }}</td>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->total_allowance_fixed }}</td>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->total_allowance_unfixed }}</td>
-            <td align="center" class="border-top border-left border-bottom">{{ $row->total_allowance_other }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->total_overtime }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->salary_in_month }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->pay_bpjs_company }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->salary_brutto }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->biaya_jabatan }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->pay_bpjs_employee }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->total_pengurangan }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->salary_netto }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->salary_1_tahun }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->ptkp_1_tahun }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->pkp }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->pph21_terhutang_1_tahun }}</td>
-            <td align="center" class="border-top border-left border-right border-bottom">{{ $row->pph21_terhutang_1_bulan }}</td>
+            <td align="left" class="border-top border-left border-bottom">{{ $row->no_employee }}</td>
+            <td align="left" class="border-top border-left border-bottom">{{ $row->name }}</td>
+            <td align="left" class="border-top border-left border-bottom">{{ $row->position_name }}</td>
+            <td align="right" class="border-top border-left border-bottom">{{ numbber_format($row->salary_pokok) }}</td>
+            <td align="right" class="border-top border-left border-bottom">{{ numbber_format($row->total_allowance_fixed) }}</td>
+            <td align="right" class="border-top border-left border-bottom">{{ numbber_format($row->total_allowance_unfixed) }}</td>
+            <td align="right" class="border-top border-left border-bottom">{{ numbber_format($row->total_allowance_other) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->total_overtime) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->salary_in_month) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->pay_bpjs_company) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->salary_brutto) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->biaya_jabatan) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->pay_bpjs_employee) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->total_pengurangan) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->salary_netto) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->salary_1_tahun) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->ptkp_1_tahun) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->pkp) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->pph21_terhutang_1_tahun) }}</td>
+            <td align="right" class="border-top border-left border-right border-bottom">{{ numbber_format($row->pph21_terhutang_1_bulan) }}</td>
         </tr>
         @endforeach
-        <tr><td  colspan="19" class="border-top border-left border-right border-bottom">Total</td><td class="border-top border-left border-right border-bottom">{{ $total->total }}</td></tr>
+        <tr><td  colspan="19" class="border-top border-left border-right border-bottom">Total</td><td class="border-top border-left border-right border-bottom" align="right">{{ number_format($total->total) }}</td></tr>
 
     </tbody>
 </table>
