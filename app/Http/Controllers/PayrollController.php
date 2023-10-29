0b663f6fe7b7b253f6932bc582ce260c241ece56
swidodo <smtwidodo04@gmail.com>
2023-10-29 12:31:15 +0700

    update

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -948,10 +948,10 @@ class PayrollController extends Controller
         $emp = explode(',',$request->employee_id);
         $employee =[];
         foreach($emp as $arr){
-            $empId = "'".$arr."'";
+            $empId = $arr;
             array_push($employee,$empId);
         }
-        // dd( $employee);
+        // ?dd( $employee);
         $data['salarys'] = DB::table('take_home_pay')
                             ->select('take_home_pay.*','employees.name as employee_name','branches.name as branch_name','position.position_name','companies.name as company_name')
                             ->leftJoin('employees','employees.id','=','take_home_pay.employee_id')
@@ -959,7 +959,7 @@ class PayrollController extends Controller
                             ->leftJoin('position','position.id','=','employees.position_id')
                             ->leftJoin('companies','companies.id','=','branches.company_id')
                             ->where('take_home_pay.branch_id',$request->branch_id)
-                            ->whereIn('take_home_pay.employee_id', [$emp])
+                            ->whereIn('take_home_pay.employee_id', $employee)
                             // ->limit(50)
                             ->get();
         $data['allowance_fixed'] = DB::select("SELECT * from get_allowance_fixed('".$request->startdate."','".$request->enddate."','".$request->branch_id."')");
