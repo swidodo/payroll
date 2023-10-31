6404db87ce0db81d36b13e72cad9b3b625ddba3a
swidodo <smtwidodo04@gmail.com>
2023-10-31 18:31:36 +0700

    update

--- resources/views/pages/contents/dashboard/filter_data/employee_status.blade.php
+++ resources/views/pages/contents/dashboard/filter_data/employee_status.blade.php
@@ -17,7 +17,7 @@
 <div id="wrapper-employement-status">
     <p class="d-block">Total &nbsp;: <span id="ides-total-employee" class="text-end">0</span></p>
     <hr>
-    <i class="fa fa-dot-circle-o text-purple me-2"></i>Permanet <span class="float-end" id="ides-jobholder">0</span><br />
+    <i class="fa fa-dot-circle-o text-purple me-2"></i>Permanent <span class="float-end" id="ides-jobholder">0</span><br />
     <i class="fa fa-dot-circle-o text-warning me-2"></i>Contract <span class="float-end" id="ides-contract">0</span><br />
     <i class="fa fa-dot-circle-o text-success me-2"></i>Probation <span class="float-end" id="ides-probation">0</span><br />
     <i class="fa fa-dot-circle-o text-success me-2"></i>Daily Worker <span class="float-end" id="ides-daily_work">0</span><br />
