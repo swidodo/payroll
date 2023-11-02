cfa6563ffc3589737b3a296a22c2ca7cf49576b0
swidodo <smtwidodo04@gmail.com>
2023-11-02 11:35:56 +0700

    update

--- app/Http/Controllers/HRM/EmployeeController.php
+++ app/Http/Controllers/HRM/EmployeeController.php
@@ -300,6 +300,7 @@ class EmployeeController extends Controller
                 $request['out_date'] = date('Y-m-d');
             }
             //total leavez
+            dd($employee->company_doj);
             if ($employee->company_doj != '' || $employee->company_doj != '-' || $employee->company_doj != null){
                 $work       = Carbon::parse($employee->company_doj)->diffInMonths(date('Y-m-d'));
                 $thisMonth  = date('m',strtotime($employee->company_doj));
