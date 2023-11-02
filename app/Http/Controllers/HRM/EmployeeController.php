d3cfb18d84f5a6af420800e1bd1c95826b29c777
swidodo <smtwidodo04@gmail.com>
2023-11-02 12:02:05 +0700

    update

--- app/Http/Controllers/HRM/EmployeeController.php
+++ app/Http/Controllers/HRM/EmployeeController.php
@@ -334,8 +334,7 @@ class EmployeeController extends Controller
             }else{
                 $request['total_leave'] = 0;
             }
-            dd($request);
-            // $employee->update($request->except(['educations', 'experiences', 'families']));
+            $employee->update($request->except(['educations', 'experiences', 'families']));
             
             //education
             
