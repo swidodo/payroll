b4769fd681f222e80a81817298d9c838b150f0fc
swidodo <smtwidodo04@gmail.com>
2023-11-02 11:55:52 +0700

    update

--- app/Http/Controllers/HRM/EmployeeController.php
+++ app/Http/Controllers/HRM/EmployeeController.php
@@ -306,17 +306,17 @@ class EmployeeController extends Controller
                 $request['out_date'] = date('Y-m-d');
             }
             //total leavez
-            if ($employee->company_doj != '' || $employee->company_doj != "-" || $employee->company_doj != null){
-                $work       = Carbon::parse($employee->company_doj)->diffInMonths(date('Y-m-d'));
-                $thisMonth  = date('m',strtotime($employee->company_doj));
-                $thisday    = date('d',strtotime($employee->company_doj));
-                $moreMonth  =  12 - (int)$thisMonth;
-            }else{
+            // if ($employee->company_doj != '' || $employee->company_doj != "-" || $employee->company_doj != null){
+            //     $work       = Carbon::parse($employee->company_doj)->diffInMonths(date('Y-m-d'));
+            //     $thisMonth  = date('m',strtotime($employee->company_doj));
+            //     $thisday    = date('d',strtotime($employee->company_doj));
+            //     $moreMonth  =  12 - (int)$thisMonth;
+            // }else{
                 $work       = date('Y-m-d');
                 $thisMonth  = date('m',strtotime($employee->company_doj));
                 $thisday    = date('d',strtotime($employee->company_doj));
                 $moreMonth  =  12 - (int)$thisMonth;
-            }
+            // }
 
             if ($work >= 12 ){
                 $total_leave =0;
