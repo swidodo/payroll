1e17a3c4be66eb9d9364aae328ea05dd8f455b44
swidodo <smtwidodo04@gmail.com>
2023-11-08 15:32:57 +0700

    update

--- resources/views/pages/contents/dashboard/dashboard-company.blade.php
+++ resources/views/pages/contents/dashboard/dashboard-company.blade.php
@@ -765,7 +765,7 @@
                     backgroundColor: '#2870B1'
                 }]
             }
-            ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
+            // ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"];
             let departchartConfig = {
                 type: 'pie',
                 backgroundColor: '#FBFCFE',
