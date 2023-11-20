8ff7fa71ebf79f12855b5eb13dc679b3b1d1520a
swidodo <smtwidodo04@gmail.com>
2023-11-21 00:50:55 +0700

    update

--- resources/views/includes/modal/reimbursement-option-modal.blade.php
+++ resources/views/includes/modal/reimbursement-option-modal.blade.php
@@ -43,7 +43,7 @@
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Edit Leave Type</h5>
+                    <h5 class="modal-title">Edit Reimbursement Type</h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
