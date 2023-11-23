6b196ac5faf5940dc7c32dbab01372504c6ff349
swidodo <smtwidodo04@gmail.com>
2023-11-24 02:36:07 +0700

    update

--- app/Http/Controllers/Report/ReportRecapAttendanceController.php
+++ app/Http/Controllers/Report/ReportRecapAttendanceController.php
@@ -74,8 +74,16 @@ class ReportRecapAttendanceController extends Controller
     public function rekap_attendance(){
         $initial = Auth::user()->initial;
         $branch = Branch::where('id',Auth::user()->branch_id)->first();
+        $emp = Employee::where('user_id',Auth::user()->id)->first();
+        
         if ($initial == "HO"){
-            $data['branch'] = Branch::where('company_id',$branch->company_id)->get();
+            if (Auth::user()->type == "company"){
+                $data['branch'] = Branch::where('company_id',$branch->company_id)->get();
+            }else{
+                $data['branch'] = AccessBranch::leftJoin('branches','branches.id','=','access_branches.branch_id')
+                                                ->where('access_branches.employee_id',$emp->id)
+                                                ->where('access_branches.company_id',$branch->company_id)->get();
+            }
         }else{
             $data['branch'] = $branch = Branch::where('id',Auth::user()->branch_id)->get();
         }
