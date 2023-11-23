8969975e2fb8b41727a47811d53bb13b9b21f515
swidodo <smtwidodo04@gmail.com>
2023-11-24 02:47:06 +0700

    update

--- app/Http/Controllers/GroupPositionController.php
+++ app/Http/Controllers/GroupPositionController.php
@@ -8,6 +8,7 @@ use App\Models\Branch;
 use App\Models\Departement;
 use App\Models\Position;
 use App\Models\Employee;
+use App\Models\AccessBranch;
 use DataTables;
 use Exception;
 use Illuminate\Support\Facades\Auth;
@@ -17,8 +18,16 @@ class GroupPositionController extends Controller
 {
     public function index(){
         $branch = Branch::where('id',Auth::user()->branch_id)->first();
+        $emp = Employee::where('user_id',Auth::user()->id)->first();
+        
         if (Auth::user()->initial == "HO"){
-            $data['branch'] = Branch::where('company_id',$branch->company_id)->get();
+            if (Auth::user()->type == "company"){
+                $data['branch'] = Branch::where('company_id',$branch->company_id)->get();
+            }else{
+                $data['branch'] = AccessBranch::leftJoin('branches','branches.id','=','access_branches.branch_id')
+                                                ->where('access_branches.employee_id',$emp->id)
+                                                ->where('access_branches.company_id',$branch->company_id)->get();
+            }
         }else{
             $data['branch'] = Branch::where('id',$branch->id)->get();
         }
