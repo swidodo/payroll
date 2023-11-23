ccefccc00dfb4dde7b94d956ac01bbb45e249a05
swidodo <smtwidodo04@gmail.com>
2023-11-24 02:15:49 +0700

    update

--- app/Http/Controllers/ThrController.php
+++ app/Http/Controllers/ThrController.php
@@ -25,7 +25,7 @@ class ThrController extends Controller
             if (Auth::user()->type == "company"){
                 $data['branch'] = Branch::select('id','name')->where('company_id',$branch->company_id)->get();
             }else{
-                $branch['branch'] = AccessBranch::leftJoin('branches','branches.id','=','access_branches.branch_id')
+                $data['branch'] = AccessBranch::leftJoin('branches','branches.id','=','access_branches.branch_id')
                                                 ->where('access_branches.employee_id',$emp->id)
                                                 ->where('access_branches.company_id',$branch->company_id)->get();
             }
