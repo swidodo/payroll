614b142927345beb1899f5b91e92fb1777a4340c
swidodo <smtwidodo04@gmail.com>
2023-11-24 00:12:51 +0700

    update

--- app/Http/Controllers/HRM/EmployeeController.php
+++ app/Http/Controllers/HRM/EmployeeController.php
@@ -962,7 +962,7 @@ class EmployeeController extends Controller
         $data['paramPph21']  = Parameter_pph21::get();
         if (Auth::user()->initial == "HO"){
             // $data['branches']      = Branch::where('company_id',$branch->company_id)->get();
-            $branch['branch'] = AccessBranch::leftJoin('branches','branches.id','=','access_branches.branch_id')
+            $branch['branches'] = AccessBranch::leftJoin('branches','branches.id','=','access_branches.branch_id')
                                                 ->where('access_branches.employee_id',$emp->id)
                                                 ->where('access_branches.company_id',$branch->company_id)->get();
             $data['position']      = Position::leftJoin('branches','branches.id','=','position.branch_id')->where('company_id',$branch->company_id)->get();
