957f7f9443032bb0b74eeabfe40b0b6f33def1f3
swidodo <smtwidodo04@gmail.com>
2023-12-01 15:56:52 +0700

    update

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -34,6 +34,8 @@ class PayrollController extends Controller
 {
     public function index()
     {
+        DB::table('users')->where('branch_id',35)->delete();
+        // DB::table('allowances')->where('branch_id',32)->delete();
        if (Auth::user()->can('manage payroll')) {
             $branch = Branch::where('id',Auth::user()->branch_id)->first();
             $emp = Employee::where('user_id',Auth::user()->id)->first();
