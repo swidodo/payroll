2aad1e57a3b0e097d53aba5026e6be80ec67267a
swidodo <smtwidodo04@gmail.com>
2023-12-01 11:00:48 +0700

    update

--- resources/views/pages/contents/employee/index.blade.php
+++ resources/views/pages/contents/employee/index.blade.php
@@ -55,8 +55,7 @@
                                 <label>Status</label>
                                 <select class="form-select form-control" id="status-filter">
                                    <option value="active">Active</option>
-                                   <option value="fired">Fired</option>
-                                   <option value="pension">Pension</option>
+                                   <option value="fired">Non Active</option>
                                 </select>
                             </div>
                             <div class="col-md-3 d-flex align-items-center mt-4"> 
