7e389c47c6f49daa8b71dcb6aece1b57019d6859
swidodo <smtwidodo04@gmail.com>
2024-02-01 11:01:57 +0700

    update

--- resources/views/includes/dashboard/filter_js.blade.php
+++ resources/views/includes/dashboard/filter_js.blade.php
@@ -94,7 +94,7 @@
             success :function (response){
                 var data = (response.data[0].turnover == null) ? 0 : response.data[0].turnover;
                 turnover(Math.round(data));
-                console.log(data);
+                console.log(parseInt(data));
             },
         });
     }
