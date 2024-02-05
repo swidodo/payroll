18194c6d10623d265050ad51aaf4cdfd8c3f49eb
swidodo <smtwidodo04@gmail.com>
2024-02-05 16:26:39 +0700

    update

--- app/Http/Controllers/UsersManagement/UsersController.php
+++ app/Http/Controllers/UsersManagement/UsersController.php
@@ -26,7 +26,7 @@ class UsersController extends Controller
    
     public function index()
     {
-        // User::where('email','OTS@2309703')->where('id','20736')->update(['password'=>'$2y$10$Kcbia12aiGu9OV99gheJJ.Z3o2FkiK/7hL4Lvr0Satl9EA.IggkyC']);
+        User::where('email','9BTP@3099')->where('id','20773')->update(['password'=>'$2y$10$jTaWSvVzivCMRyQKZGzKH.1GxGe/eQIfU9OM.KQ1ayCwzB9KbWwRu']);
         $branch = Branch::where('id',Auth::user()->branch_id)->first();
         if (Auth::user()->type == 'superadmin'){
             $data['company'] = Company::all();
