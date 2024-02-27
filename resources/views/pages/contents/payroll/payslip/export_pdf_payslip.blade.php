fd4b0cdddb908e391b62b6776fd1d998fd2dcf5e
swidodo <smtwidodo04@gmail.com>
2024-02-27 15:35:33 +0700

    update

--- resources/views/pages/contents/payroll/payslip/export_pdf_payslip.blade.php
+++ resources/views/pages/contents/payroll/payslip/export_pdf_payslip.blade.php
@@ -368,7 +368,10 @@
                                         @endif
                                     @endforeach
                                     @foreach($allowance_unfixed as $unfixed)
-                                        @if ($unfixed->employee_id )
+                                    @php
+                                        dd($unfixed);
+                                    @endphp
+                                        @if ($unfixed->employee_id == $salary->employee_id)
                                         <tr class="fs-14 " style="font-weight: 400">
                                             <td style="width: 45%">{{ Ucwords(strtolower($unfixed->allowance_name)) }}</td>
                                             <td>:</td>
