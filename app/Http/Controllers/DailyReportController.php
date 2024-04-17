7e9d4a34aee1ab3abef8bee962961923877edf54
swidodo <smtwidodo04@gmail.com>
2024-04-18 01:15:24 +0700

    update

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -26,12 +26,12 @@ class DailyReportController extends Controller
                     'employees.name as employee_name')
                     ->leftJoin('employees','employees.id','=','v_daily_reports.employee_id')
                     ->where('v_daily_reports.department_id',$request->department_id);
-                    if ($request->startdate != "" && $request->enddate != ""){
-                        $data->whereBetween('v_daily_reports.date',[$request->startdate,$request->enddate]);
-                    }
-                    if ($request->employee_id != ""){
-                        $data->where('v_daily_reports.employee_id',$request->employee_id);
-                    }
+                    // if ($request->startdate != "" && $request->enddate != ""){
+                    //     $data->whereBetween('v_daily_reports.date',[$request->startdate,$request->enddate]);
+                    // }
+                    // if ($request->employee_id != ""){
+                    //     $data->where('v_daily_reports.employee_id',$request->employee_id);
+                    // }
                     $data->orderBy('date','DESC');
                     $data->get();
         return DataTables::of($data)
