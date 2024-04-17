dadf30304d0097671537073b84260159b5de5ef3
swidodo <smtwidodo04@gmail.com>
2024-04-18 02:21:58 +0700

    update

--- app/Exports/DailyReportExport.php
+++ app/Exports/DailyReportExport.php
@@ -19,12 +19,12 @@ class DailyReportExport implements FromView
          $data = DB::table('v_rekap_daily_report')
                     ->where('branch_id','=',Auth::user()->branch_id)
                     ->whereBetween('date',[$this->request->startdate,$this->request->enddate]);
-                    if($this->request->department_id !='all'){
-                        $data->where('department_id',$this->request->department_id);
-                    }
-                    if ($this->request->employee_id !='all'){
-                        $data->where('department_id',$this->request->employee_id);
-                    }
+                    // if($this->request->department_id !='all'){
+                    //     $data->where('department_id',$this->request->department_id);
+                    // }
+                    // if ($this->request->employee_id !='all'){
+                    //     $data->where('department_id',$this->request->employee_id);
+                    // }
         $res['daily'] = $data->get(); 
         dd( $res);
         return view('pages.contents.report.daily_report.export_daily_report',$res);
