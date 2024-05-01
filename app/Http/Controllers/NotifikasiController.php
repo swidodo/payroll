3ca5b2cec60432c65b10a2a9132d78e90fd5ccdc
swidodo <smtwidodo04@gmail.com>
2024-05-01 20:10:22 +0700

    update

--- app/Http/Controllers/NotifikasiController.php
+++ app/Http/Controllers/NotifikasiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Message;
 use Kreait\Firebase\Messaging\Notification;
 use App\Models\User;
 class NotifikasiController extends Controller
--- vendor/kutia-software-company/larafirebase/src/Helpers/All.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!function_exists('larafirebase')) {
-    function larafirebase()
-    {
-        return app('larafirebase');
-    }
-}
