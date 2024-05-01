650584b6dc84acd9ab4f7a784dcfc49d1cfa1058
swidodo <smtwidodo04@gmail.com>
2024-05-01 15:34:09 +0700

    update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
-
-
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 class AuthController extends Controller
 {
     /**
