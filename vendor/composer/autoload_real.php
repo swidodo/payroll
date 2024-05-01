15d606652c66828d5d87c5923bb9df3445ea31a8
swidodo <smtwidodo04@gmail.com>
2024-05-01 21:28:23 +0700

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInita25205f5b5e9d31dd55ddd7df050dc5c
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInita25205f5b5e9d31dd55ddd7df050dc5c::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
