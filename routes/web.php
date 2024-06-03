470924ceeab2b5f153c67a10931db232e21dc1e0
swidodo <smtwidodo04@gmail.com>
2024-06-03 21:36:03 +0700

    updt

--- routes/web.php
+++ routes/web.php
@@ -670,7 +670,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('action-need',[ApprovalController::class,'list_approval']);
         //  push notif
         Route::get('/notif', [NotifikasiController::class, 'sendNotification']);
-        Route::post('/fcm-token', [NotifikasiController::class, 'updateToken'])->name('fcmToken');
+        // Route::post('/fcm-token', [NotifikasiController::class, 'updateToken'])->name('fcmToken');
         Route::get('/send-notification',[NotifikasiController::class,'notification']);
 
 });
