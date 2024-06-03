c69ad6daa85be2c205dc5746fba53f82db718399
swidodo <smtwidodo04@gmail.com>
2024-06-03 17:46:19 +0700

    p

--- app/Http/Controllers/NotifikasiController.php
+++ app/Http/Controllers/NotifikasiController.php
@@ -1,19 +1,18 @@
 <?php
-
 require 'vendor/autoload.php';
 
 use Google\Client as Google_Client;
 
 // Buat instance Google_Client
 $googleClient = new Google_Client();
-$googleClient->setAuthConfig(storage_path('/home/pehadirm/public_html/storage/app/pehadir-1f207-8c712bfe1daa.json'));
+$googleClient->setAuthConfig('pehadir-1f207-8c712bfe1daa.json');
 $googleClient->addScope('https://www.googleapis.com/auth/cloud-platform');
 
 // Dapatkan access token
 $accessToken = $googleClient->fetchAccessTokenWithAssertion()["access_token"];
-
+print_r($accessToken);die();
 // Siapkan data notifikasi
-$url = "https://fcm.googleapis.com/v1/projects/YOUR_PROJECT_ID/messages:send";
+$url = "https://fcm.googleapis.com/v1/projects/pehadir-1f207/messages:send";
 $data = [
     'message' => [
         'token' => 'DEVICE_REGISTRATION_TOKEN',
