e293a0f5d7ffaa161b51cce06997412f3bed3362
swidodo <smtwidodo04@gmail.com>
2024-06-03 16:06:08 +0700

    update

--- app/Http/Controllers/NotifikasiController.php
+++ app/Http/Controllers/NotifikasiController.php
@@ -1,84 +1,50 @@
 <?php
 
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Kreait\Firebase\Messaging\Message;
-use Kreait\Firebase\Messaging\Notification;
-use Kreait\Firebase\Messaging\CloudMessage;
-
-//
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Kutia\Larafirebase\Messages\FirebaseMessage;
 require 'vendor/autoload.php';
 
-use App\Models\User;
-use GuzzleHttp\Client as Google_Client;
-// use Google_Client;
+use Google\Client as Google_Client;
 
-class NotifikasiController extends Controller
-{
-    private $client;
-    private $project_id;
-    private $access_token;
+// Buat instance Google_Client
+$googleClient = new Google_Client();
+$googleClient->setAuthConfig(storage_path('/home/pehadirm/public_html/storage/app/pehadir-1f207-8c712bfe1daa.json'));
+$googleClient->addScope('https://www.googleapis.com/auth/cloud-platform');
 
-    public function __construct()
-    {
-        $this->project_id = 'pehadir-1f207';
+// Dapatkan access token
+$accessToken = $googleClient->fetchAccessTokenWithAssertion()["access_token"];
 
-        $googleClient = new Google_Client();
-        $googleClient->setAuthConfig(storage_path('/home/pehadirm/public_html/storage/app/pehadir-1f207-8c712bfe1daa.json'));
-        $googleClient->addScope('https://www.googleapis.com/auth/cloud-platform');
+// Siapkan data notifikasi
+$url = "https://fcm.googleapis.com/v1/projects/YOUR_PROJECT_ID/messages:send";
+$data = [
+    'message' => [
+        'token' => 'DEVICE_REGISTRATION_TOKEN',
+        'notification' => [
+            'title' => 'Hello',
+            'body' => 'World'
+        ],
+        'data' => [
+            'key1' => 'value1',
+            'key2' => 'value2'
+        ]
+    ]
+];
 
-        $this->access_token = $googleClient->fetchAccessTokenWithAssertion()["access_token"];
-        $this->client = new Client();
-    }
+// Siapkan permintaan cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer ' . $accessToken,
+    'Content-Type: application/json'
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
+// Eksekusi permintaan dan dapatkan respons
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
 
-    public function index(){
-        return $access_token;
-    }
-    public function updateToken(Request $request){
-        try{
-            // dd($request->token);
-            User::where('branch_id',28)->update(['fcm_token'=>$request->token]);
-            return response()->json([
-                'success'=>$request->token
-            ]);
-        }catch(Exception $e){
-            report($e);
-            return response()->json([
-                'success'=>false
-            ],500);
-        }
-    }
-    public function sendNotification($token, $title, $body, $data = [])
-    {
-        $url = "https://fcm.googleapis.com/v1/projects/{$this->project_id}/messages:send";
-
-        $message = [
-            'message' => [
-                'token' => $token,
-                'notification' => [
-                    'title' => $title,
-                    'body' => $body
-                ],
-                'data' => $data
-            ]
-        ];
-
-        $response = $this->client->post($url, [
-            'headers' => [
-                'Authorization' => 'Bearer ' . $this->access_token,
-                'Content-Type' => 'application/json'
-            ],
-            'json' => $message
-        ]);
-
-        return json_decode($response->getBody(), true);
-    }
-    
-    
-    }
-
+// Cetak respons
+echo $response;
